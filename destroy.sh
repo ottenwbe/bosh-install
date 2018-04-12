435856eac530fe3eeda4b33d2696ebb75b216b4a
Wes <wfowlks@users.noreply.github.com>
2018-04-12 11:15:20 -0400

    Refactor AZ & Region

--- destroy.sh
+++ destroy.sh
@@ -10,6 +10,8 @@ internal_cidr=$(terraform output bosh_subnet_cidr)
 internal_gw=$(terraform output bosh_gw)
 subnet_id=$(terraform output bosh_subnet)
 bosh_ip=$(terraform output bosh_ip)
+aws_az=$(terraform output aws_az)
+aws_region=$(terraform output aws_region)
 # Read the aws access key and secret key
 while read -r line; do declare $line; done <terraform.tfvars
 
@@ -18,7 +20,7 @@ scp -oStrictHostKeyChecking=no -i ssh/deployer.pem ec2/delete.sh ubuntu@${jumpbo
 ssh -oStrictHostKeyChecking=no -i ssh/deployer.pem ubuntu@${jumpbox_dns} << EOF
   echo "The bosh director will be destroyed now"
   chmod +x delete.sh
-  ./delete.sh "${internal_cidr}" "${internal_gw}" "${bosh_ip}" ${access_key} ${secret_key} "${subnet_id}"  ~/.ssh/bosh.pem
+  ./delete.sh "${internal_cidr}" "${internal_gw}" "${bosh_ip}" ${access_key} ${secret_key} "${subnet_id}" "${aws_az}" "${aws_region}" ~/.ssh/bosh.pem
 EOF
 
 # Destroy the terraform resources
